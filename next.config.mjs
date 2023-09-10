c93142290bce6c108d29bb769b0896acd3435ccc
rajput-hemant <rajput.hemant2001@gmail.com>
2023-09-10 15:13:12 +0530

    feat(env): add upstash redis env vars
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+import "./src/lib/env.mjs";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* ... */
+};
+
+export default nextConfig;
---
+++ src/lib/env.mjs
@@ -0,0 +1,9 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    UPSTASH_REDIS_REST_URL: z.string().url(),
+    UPSTASH_REDIS_REST_TOKEN: z.string().min(1),
+  },
+});
