e89f188bb39491054013b31138591753b75b58b4
rajput-hemant <rajput.hemant2001@gmail.com>
2023-10-16 01:04:13 +0530

    feat(auth): add `middleware` to protect sensitive routes
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

---
+++ src/middleware.ts
@@ -0,0 +1,47 @@
+import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth(
+  async function middleware(req) {
+    const pathname = req.nextUrl.pathname;
+
+    /* -----------------------------------------------------------------------------------------------
+     * manages route protecion
+     * -----------------------------------------------------------------------------------------------*/
+    const isAuth = await getToken({ req });
+    const isLoginPage = pathname.startsWith("/login");
+
+    const sensitiveRoutes = ["/dashboard"];
+    const isAccessiingSensitiveRoute = sensitiveRoutes.some((route) =>
+      pathname.startsWith(route)
+    );
+
+    if (isLoginPage) {
+      if (isAuth) {
+        return NextResponse.redirect(new URL("/dashboard", req.url));
+      }
+
+      return NextResponse.next();
+    }
+
+    if (!isAuth && isAccessiingSensitiveRoute) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+
+    if (pathname === "/") {
+      return NextResponse.redirect(new URL("/dashboard", req.url));
+    }
+  },
+  {
+    callbacks: {
+      async authorized() {
+        return true;
+      },
+    },
+  }
+);
+
+export const config = {
+  matcher: ["/", "/login", "/dashboard/:path*"],
+};
